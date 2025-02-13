f5fb71c53f5904439abb7b5e7013c8df3efdb8a0
R.R <rekamario33@gmail.com>
2025-02-14 00:06:24 +0100

    1

--- KAndH/src/app/components/home/home.component.ts
+++ KAndH/src/app/components/home/home.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import { NgForm, FormsModule } from '@angular/forms'; 
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [FormsModule], // Add FormsModule here
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  private readonly SERVICE_ID = environment.emailjs.SERVICE_ID;
+  private readonly TEMPLATE_ID = environment.emailjs.TEMPLATE_ID;
+  private readonly USER_ID = environment.emailjs.USER_ID;
 
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      const formData = form.value;
+
+      emailjs.send(this.SERVICE_ID, this.TEMPLATE_ID, formData, this.USER_ID)
+        .then((response: EmailJSResponseStatus) => {
+          console.log('Email sent successfully!', response);
+          alert('Email sent successfully!');
+          form.reset();
+        })
+        .catch((error: any) => {
+          console.error('Error sending email:', error);
+          alert('Failed to send email. Please try again.');
+        });
+    }
+  }
 }
---
+++ KAndH/src/environments/environment.ts
@@ -0,0 +1,9 @@
+//environment.ts
+export const environment = {
+    production: false,
+    emailjs: {
+      SERVICE_ID: 'service_f2fbkqu', // Your Service ID
+      TEMPLATE_ID: 'template_dbyaaei', // Your Template ID
+      USER_ID: 'n8vJes38AN8MG0Kz8', // Your User ID
+    },
+  };
